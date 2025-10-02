3b0ac44e64d931566d878287f211444f777db939
Taimiya Amjad <taimiyaamjad0@gmail.com>
2025-10-02 11:13:08 +0000

    Take the keys from the env and harcode it and remove the env file then

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,15 +1,14 @@
-
 import { initializeApp, getApps, getApp, FirebaseApp } from "firebase/app";
 import { getAuth, Auth } from "firebase/auth";
 import { getFirestore, Firestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyA_...REPLACE_WITH_YOUR_KEY",
+  authDomain: "your-project-id.firebaseapp.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project-id.appspot.com",
+  messagingSenderId: "123456789012",
+  appId: "1:123456789012:web:abcdef1234567890abcdef",
 };
 
 function getFirebaseInstances() {
