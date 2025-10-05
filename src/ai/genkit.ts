5b59722d9db5c8a04d6fe066ff1f78a034000fc5
Taimiya Amjad <taimiyaamjad0@gmail.com>
2025-10-05 04:55:32 +0000

    Chnagw the gemini api key to  AIzaSyCpVcAmZ8GkGtxXXSz-mYT6A98x0WZTdA0

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
-  plugins: [googleAI({apiKey: 'AIzaSyCSLxsDsHHe4HU92Vx47ImBghjQvsZ_sjs'})],
+  plugins: [googleAI({apiKey: 'AIzaSyCpVcAmZ8GkGtxXXSz-mYT6A98x0WZTdA0'})],
   model: 'googleai/gemini-2.5-flash',
 });
